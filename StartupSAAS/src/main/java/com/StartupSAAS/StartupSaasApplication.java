85dd0e96fe532ba280c822aff16de9327417a074
Rehana Rafiaah <rehanaa2102@gmail.com>
2026-06-10 15:59:04 +0600

    Project Folder Initialized

---
+++ StartupSAAS/src/main/java/com/StartupSAAS/StartupSaasApplication.java
@@ -0,0 +1,13 @@
+package com.StartupSAAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupSaasApplication.class, args);
+	}
+
+}
---
+++ StartupSAAS/src/test/java/com/StartupSAAS/StartupSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StartupSAAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
