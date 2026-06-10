58ea787eabe64ce59fec5e7aac26f1f271dd76b8
Rehana Akter <rehanaa2102@gmail.com>
2026-06-11 04:04:25 +0600

    Add

---
+++ Butterfly/src/test/java/com/Butterfly/ButterflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Butterfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButterflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
